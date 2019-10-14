17ea5c2c480c5f499afa7fbeacaa2d60c0b986d5
Enaho Murphy <enahomurphy@gmail.com>
2019-10-15 00:08:42 +0100

    Add collision detection for circle in reactangle

--- src/canvas/Rectangle.js
+++ src/canvas/Rectangle.js
@@ -28,6 +28,11 @@ class Rectangle {
     return false;
   }
 
+
+  // Is the RIGHT edge of r1 to the RIGHT of the LEFT edge of r2?
+  // Is the LEFT edge of r1 to the LEFT of the RIGHT edge of r2?
+  // Is the BOTTOM edge of r1 BELOW the TOP edge of r2?
+  // Is the TOP edge of r1 ABOVE the BOTTOM edge of r2?
   rectangleInside(react) {
     if (
       this.position.x < react.position.x + react.width &&
@@ -39,6 +44,27 @@ class Rectangle {
     }
 
     return false;
+  }
+
+  // If the circle is to the RIGHT of the square, check against the RIGHT edge.
+  // If the circle is to the LEFT of the square, check against the LEFT edge.
+  // If the circle is ABOVE the square, check against the TOP edge.
+  // If the circle is to the BELOW the square, check against the BOTTOM edge.
+  circleInRectangle(circle) {
+    let x = circle.position.x;
+    let y = circle.position.y;
+
+    if (circle.position.x < this.position.x) x = this.position.x;
+    else if (circle.position.x > this.position.x + this.width) x = this.position.x + this.width;
+
+    if (circle.position.y < this.position.y) y = this.position.y;
+    else if (this.position.y > this.position.y + this.height) y = this.position.x + this.width;
+
+    const distX = circle.position.x - x;
+    const distY = circle.position.y - y;
+    const distance = Math.hypot(distX, distY);
+
+    return (distance <= circle.radius);
   }
 }
 
